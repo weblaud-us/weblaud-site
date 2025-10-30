68b29c5d4821926e30281ab7fdaf97a6fedc9916
Shuvo Chandra Deb Nath <shuvochandra999@gmail.com>
2025-10-30 22:35:15 +0600

    our track section added

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -44,16 +44,13 @@ const Footer: React.FC = () => {
   return (
     <footer className="bg-black text-white py-10">
       <div className="max-w-7xl border-t border-light-black mx-auto py-4 px-4 md:py-10 md:px-16">
-        {/* Top Section: Logo, Menu, Social */}
         <div className="flex flex-col lg:flex-row justify-between items-center gap-8">
-          {/* Logo */}
           <div className="text-center lg:text-left">
             <h2 className="text-2xl md:text-3xl font-semibold font-poppins">
               <span className="text-blue-500">Dev</span> Nest
             </h2>
           </div>
 
-          {/* Navigation Menu */}
           <div className="text-center lg:text-left">
             <ul className="flex flex-wrap justify-center lg:flex-row font-barlow gap-4 md:gap-6 font-medium">
               {menuItems.map((item) => (
@@ -67,7 +64,6 @@ const Footer: React.FC = () => {
             </ul>
           </div>
 
-          {/* Social Links */}
           <div className="flex justify-center lg:justify-end gap-3 border border-light-black p-4 rounded-xl">
             {socialLinks.map(({ icon, url }, index) => (
               <IconTile key={index} href={url} ariaLabel={`Visit ${url}`}>
@@ -77,9 +73,7 @@ const Footer: React.FC = () => {
           </div>
         </div>
 
-        {/* Middle Section: Contact Info & Legal Links */}
         <div className="flex flex-col lg:flex-row items-center justify-between mt-10 gap-6">
-          {/* Contact Info */}
           <div className="flex flex-col sm:flex-row gap-4 sm:gap-6 items-center text-sm">
             {contactItems.map(({ icon, text }) => (
               <div
@@ -91,8 +85,6 @@ const Footer: React.FC = () => {
               </div>
             ))}
           </div>
-
-          {/* Legal Links */}
           <div className="flex gap-4 flex-wrap justify-center">
             {legalLinks.map(({ label, href }) => (
               <a
@@ -106,7 +98,6 @@ const Footer: React.FC = () => {
           </div>
         </div>
 
-        {/* Bottom Section: Copyright */}
         <div className="flex flex-col md:flex-row justify-center font-barlow items-center text-sm text-dark-gray mt-10 pt-4">
           <p className="text-center">Design By Hridoy Hossain 2025 (Mkt)</p>
         </div>
--- app/routes/aboutUs.tsx
+++ app/routes/aboutUs.tsx
@@ -1,9 +1,11 @@
 import Discuss from "~/components/discuss";
+import OurTrack from "~/components/ourTrack";
 import TheTeam from "~/components/theTeam";
 
 const AboutUs = () => {
   return (
     <div>
+      <OurTrack />
       <TheTeam />
       <Discuss />
     </div>
